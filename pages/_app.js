8d6d4f71b9dff87cf4c52cfc2cd0c4025ea8a050
Jeff Everhart <Jeffeverhart383@gmail.com>
2022-04-06 13:34:07 -0400

    remove apollo provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,8 @@
-import { ApolloProvider } from "@apollo/client/react";
-import { client } from "../lib/apollo";
-
 import '../styles/index.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-      <ApolloProvider client={client}>
         <Component {...pageProps} />
-      </ApolloProvider>
     )
 }
 
